742e27c635fdf7aa5318453a061b5edfa76ed166
natalyabqr-star <natalyabqr@gmail.com>
2026-09-17 11:38:57 -0300

    Exercício do cachorro

---
+++ aula16.09/cachorro.py
@@ -0,0 +1,15 @@
+class Cachorro:
+    """Representa um cachorro"""
+    pass
+
+
+rex = Cachorro()
+bob = Cachorro()
+
+print(type(rex))
+print(rex)
+
+print(type(bob))
+print(bob)
+
+print(rex is bob)
